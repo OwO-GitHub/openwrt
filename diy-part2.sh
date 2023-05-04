3a382a0ac6bfb4b9f40a718944267c518f16efc6
OwO-GitHub <79355200+OwO-GitHub@users.noreply.github.com>
2023-05-05 00:53:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
